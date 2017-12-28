105b0b174169be08a23cc69da52a78f92fb4ff58
vijayvarmaD <vjvarma@outlook.in>
2017-12-28 08:04:27 +0530

    order data req changes

--- controllers/Orders.js
+++ controllers/Orders.js
@@ -2,6 +2,8 @@ const Customer = require('../models/CustomerSchema');
 const Vendor = require('../models/VendorSchema');
 const DeliveryPerson = require('../models/DeliveryPersonSchema');
 const OrderData = require('../models/OrderDataSchema');
+const ProductData = require('../models/ProductDataSchema');
+const Transactions = require('../models/TransactionSchema');
 
 module.exports = {
     orderDataFromId: async (req, res, next) => {
@@ -10,11 +12,36 @@ module.exports = {
             const { oId } = req.value.body;
             const vendor = req.user._id;
 
+            // Response body values
+            let resData;
+
             // Find OrderData from db
-            const orderData = await OrderData.findById(oId, { transactionId: 0 });
+            let orderData = await OrderData.findById(oId);
             if(!orderData) {
                 return res.status(403).json({ error: 'The order is you requested is not found.' });
             }
+
+            // Attach product details
+            orderData.products.forEach(element => {
+                const orderInfo = await ProductData.findById(element.productId, { name: 1, veg: 1 });
+                element.name = orderInfo.name;
+                element.veg = orderInfo.veg;
+            });
+
+            // Attach order value 
+            const { value } = await Transactions.findById(orderData.transactionId, { value: 1 });
+            if(!value) {
+                return res.status(403).json({ error: 'Unable to find order value' });
+            }
+            orderData.orderAmount = value;
+
+            // Attach Delivery Person details
+            const dpDetails = await DeliveryPerson.findById(orderData.deliveryPerson, { phone: 1, name: 1 });
+            if(!dpDetails) {
+                return res.status(403).json({ error: 'Unable to find deliveryPerson details' });
+            }
+            orderData.deliveryPerson = dpDetails;
+
             res.status(200).json({ orderData });
         } catch (error) {
             next(error);
